494d4e98e78c40eb02762a5d50c3aded13180015
Christian Perez Llamas <christian.perez@schibsted.com>
2018-12-06 12:45:00 +0100

    preparing to merge

--- ci/build-docs.sh
+++ ci/build-docs.sh
@@ -4,4 +4,5 @@ set -ex
 
 cargo doc
 
-echo "<meta http-equiv=refresh content=0;url=`echo $TRAVIS_REPO_SLUG | cut -d '/' -f 2`/index.html>" > target/doc/index.html
+REPO_NAME=$(echo $TRAVIS_REPO_SLUG | cut -d '/' -f 2)
+echo "<meta http-equiv=refresh content=0;url=$REPO_NAME/index.html>" > target/doc/index.html
