c5cd73c8dc400d1feaa0b9654d70e3f69f700096
Jason Grlicky <jason@paracosm.us>
2020-05-18 12:23:01 -0700

    Fixed notifications example by starting the thread's run loop

--- examples/notifications.rs
+++ examples/notifications.rs
@@ -1,16 +1,39 @@
 extern crate coremidi;
+extern crate core_foundation;
+
+use coremidi::{
+    Client,
+    Notification,
+};
+
+use core_foundation::runloop::{
+    CFRunLoopRunInMode,
+    kCFRunLoopDefaultMode,
+};
 
 fn main() {
-    println!("Logging Client Notifications");
-    println!("Press Enter to Finish");
+    println!("Logging MIDI Client Notifications");
+    println!("Will Quit Automatically After 10 Seconds");
     println!("");
 
-    let _client = coremidi::Client::new_with_notifications("example-client", print_notification).unwrap();
+    let _client = Client::new_with_notifications("example-client", print_notification).unwrap();
 
-    let mut input_line = String::new();
-    std::io::stdin().read_line(&mut input_line).ok().expect("Failed to read line");
+    // As the MIDIClientCreate docs say (https://developer.apple.com/documentation/coremidi/1495360-midiclientcreate),
+    // notifications will be delivered on the run loop that was current when
+    // Client was created.
+    //
+    // In order to actually receive the notifications, a run loop must be
+    // running. Since this sample app does not use an app framework like 
+    // UIApplication or NSApplication, it does not have a run loop running yet.
+    // So we start one that lasts for 10 seconds with the following line.
+    //
+    // You may not have to do this in your app - see https://developer.apple.com/library/archive/documentation/Cocoa/Conceptual/Multithreading/RunLoopManagement/RunLoopManagement.html#//apple_ref/doc/uid/10000057i-CH16-SW24
+    // for information about when run loops are running automatically.
+    unsafe {
+        CFRunLoopRunInMode(kCFRunLoopDefaultMode, 10.0, 0)
+    };
 }
 
-fn print_notification(notification: &coremidi::Notification) {
+fn print_notification(notification: &Notification) {
     println!("Received Notification: {:?} \r", notification);
 }
